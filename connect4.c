bbe02b13980e3a53e6bcb9803b2ab87cbda4732c
Kah Gene Leong <kahgeneleong@airasia.com>
2020-12-07 00:16:10 +0000

    Add functions to setup board and handle player moves

--- connect4.c
+++ connect4.c
@@ -3,7 +3,11 @@
 #include"connect4.h"
 
 struct board_structure {
- // need to keep track positions of 'x' and 'o'
+	char **board_ptr;
+	int max_rows;
+	int max_cols;
+	int num_of_x;
+	int num_of_o;
 };
 
 board setup_board(){
@@ -11,46 +15,58 @@ board setup_board(){
 	if (board_to_play == NULL){
 		exit(1);
 	}
+	board_to_play->max_rows = 1;
+	board_to_play->max_cols = 4;
+	board_to_play->board_ptr = (char**)malloc((board_to_play->max_rows) * sizeof(char*)); // min 1 row 
+	if(board_to_play->board_ptr == NULL){
+		exit(1);
+	}
+	(board_to_play->board_ptr)[0] = (char*)malloc((board_to_play->max_cols) * sizeof(char)); // min 4 cols
+	if((board_to_play->board_ptr)[0] == NULL){
+		exit(1);
+	}
 	return board_to_play;
 }
 
-void cleanup_board(board u){
-//You may put code here
+// void cleanup_board(board u){
+// //You may put code here
+// }
+
+void print_board(board u){
+	for(int x = 0; x < u->max_rows; x++){
+		printf("\n");
+		for(int y = 0; y < u->max_cols; y++){
+			printf("%c", (u->board_ptr)[x][y]);
+		}
+	}
+	printf("\n\n");
 }
 
 void read_in_file(FILE *infile, board u){
-	char buffer[520];
+	char buffer[520]; // need to realloc this somewhere just in case
 	int count = 0;
-	int max_rows = 1;
-	int max_cols = 4;
-	char **board_ptr;
 	int curr_row = 0;
 	int curr_col = 0;
-	int first_loop = 1;
-	FILE *infile=fopen("initial_board.txt","r");
 	
-	board_ptr = (char**)malloc(1 * sizeof(char*)); // min 1 row 
-	board_ptr[0] = (char*)malloc(4 * sizeof(char)); // min 4 cols
-
 	while(!feof(infile)){
-		if(curr_row == max_rows){
-			board_ptr = (char**)realloc(board_ptr, (max_rows+1)*sizeof(*board_ptr));
-			if(board_ptr == NULL){
+		if(curr_row == u->max_rows){
+			u->board_ptr = (char**)realloc(u->board_ptr, ((u->max_rows)+1)*sizeof(*(u->board_ptr)));
+			if(u->board_ptr == NULL){
 				exit(1);
 			}
-			max_rows++;
-			board_ptr[curr_row] = (char*)malloc(max_cols*sizeof(char));
+			u->max_rows++;
+			(u->board_ptr)[curr_row] = (char*)malloc((u->max_cols)*sizeof(char));
 		}
 		fscanf(infile, "%c", &buffer[count]);
 		if(buffer[count] == '.' || buffer[count] == 'x' || buffer[count] == 'o'){
-			if(curr_col == max_cols){
-				board_ptr[curr_row] = (char*)realloc(board_ptr[curr_row], (max_cols+1)*sizeof(char));
-				if(board_ptr[curr_row] == NULL){
+			if(curr_col == u->max_cols){
+				(u->board_ptr)[curr_row] = (char*)realloc((u->board_ptr)[curr_row], ((u->max_cols)+1)*sizeof(char));
+				if((u->board_ptr)[curr_row] == NULL){
 					exit(1);
 				}
-				max_cols++;
+				u->max_cols++;
 			}
-			board_ptr[curr_row][curr_col] = buffer[count];
+			(u->board_ptr)[curr_row][curr_col] = buffer[count];
 			curr_col++;
 		}else{
 			curr_row++;
@@ -59,19 +75,7 @@ void read_in_file(FILE *infile, board u){
 		count++;
 	}
 
-	max_rows--;
-
-
-	// for(int i = 0; i < max_rows; i++){
-	// 	printf("\n");
-	// 	for(int j = 0; j < max_cols; j++){
-	// 		printf("%c", board_ptr[i][j]);
-	// 	}
-	// }
-	// printf("\n\n");
-	// printf("Total rows: %d  Total columns: %d\n", max_rows, max_cols);
-
-	fclose(infile);
+	(u->max_rows)--;
 }
 
 void write_out_file(FILE *outfile, board u){
@@ -79,31 +83,44 @@ void write_out_file(FILE *outfile, board u){
 }
 
 char next_player(board u){
-//You may put code here
+	if(u->num_of_x == 0 && u->num_of_o == 0) return "x";
+	if(u->num_of_x > u->num_of_o){
+		return "o";
+	}else{
+		return "x";
+	}
 }
 
-char current_winner(board u){
-//You may put code here
-}
+// char current_winner(board u){
+// //You may put code here
+// }
 
 struct move read_in_move(board u){
-//You may put code here
-  printf("Player %c enter column to place your token: ",next_player(u)); //Do not edit this line
-//You may put code here
-  printf("Player %c enter row to rotate: ",next_player(u)); // Do not edit this line
-//You may put code here
+	struct move{
+		int chosen_col;
+		int chosen_rotation;
+	};
+
+	struct player_move = malloc(sizeof(struct move));
+	printf("Player %c enter column to place your token: ",next_player(u)); //Do not edit this line
+	scanf("%d", &(player_move->chosen_col));
+	printf("Player %c enter row to rotate: ",next_player(u)); // Do not edit this line
+	scanf("%d", &(player_move->chosen_rotation));
+
+	return player_move;
 }
 
 int is_valid_move(struct move m, board u){
-//You may put code here
+	if (m->chosen_col < 1 || m->chosen_col > u->max_cols || m->chosen_rotation < -(u->max_rows) || m->chosen_rotation > u->max_rows) return 0;
+	if ((u->board_ptr)[0][(m->chosen_col)-1] == ".") return 0;
 }
 
-char is_winning_move(struct move m, board u){
-//You may put code here
-}
+// char is_winning_move(struct move m, board u){
+// //You may put code here
+// }
 
-void play_move(struct move m, board u){
-//You may put code here
-}
+// void play_move(struct move m, board u){
+// //You may put code here
+// }
 
-//You may put additional functions here if you wish.
+

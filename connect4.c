fac0586a0df9300850b151e973b28a4aa92c3e15
KGene1901 <kgleong2001@gmail.com>
2021-02-07 00:49:43 +0000

    Fix memory leakage

--- connect4.c
+++ connect4.c
@@ -8,18 +8,24 @@ struct board_structure{
 	int max_cols;
 	int num_of_x;
 	int num_of_o;
-	int *win_info;
+	int *win_info; // {x.x, x.y, x.dir, o.x, o.y, o.dir}
 };
 
 board setup_board(){
 	board board_to_play = malloc(sizeof(struct board_structure));
-	if (board_to_play == NULL) exit(1);
+	if (board_to_play == NULL) {
+		fprintf(stderr, "Error: Unable to allocate memory for board struct\n");
+		exit(1);
+	}
 
 	board_to_play->max_rows = 1;
 	board_to_play->max_cols = 4;
 
-	board_to_play->win_info = (int*)malloc(6*sizeof(int)); // {x.x, x.y, x.dir, o.x, o.y, o.dir}
-	if(board_to_play->win_info == NULL) exit(1);
+	board_to_play->win_info = (int*)malloc(6*sizeof(int)); 
+	if(board_to_play->win_info == NULL){
+		fprintf(stderr, "Error: Unable to allocate memory for winning information\n");
+		exit(1);
+	}
 
 	board_to_play->win_info[0] = -1;
 	board_to_play->win_info[1] = -1;
@@ -29,10 +35,25 @@ board setup_board(){
 	board_to_play->win_info[5] = -1;
 
 	board_to_play->board_ptr = (char**)malloc((board_to_play->max_rows) * sizeof(char*)); // min 1 row 
-	if(board_to_play->board_ptr == NULL) exit(1);
+	if(board_to_play->board_ptr == NULL){
+		fprintf(stderr, "Error: Unable to allocate memory for board pointer\n");
+		exit(1);
+	}
 
 	(board_to_play->board_ptr)[0] = (char*)malloc((board_to_play->max_cols) * sizeof(char)+1); // min 4 cols
-	if((board_to_play->board_ptr)[0] == NULL) exit(1);
+	if((board_to_play->board_ptr)[0] == NULL){
+		fprintf(stderr, "Error: Unable to allocate memory for pointer to char\n");
+		free(board_to_play->board_ptr);
+		exit(1);
+	}
+
+	board_to_play->board_ptr[0][0] = ' ';
+	board_to_play->board_ptr[0][1] = ' ';
+	board_to_play->board_ptr[0][2] = ' ';
+	board_to_play->board_ptr[0][3] = ' ';
+
+	board_to_play->num_of_x = 0;
+	board_to_play->num_of_o = 0;
 	
 	return board_to_play;
 }
@@ -45,25 +66,38 @@ void cleanup_board(board u){
 }
 
 void read_in_file(FILE *infile, board u){
-	char symbol[2];
-	int count = 0;
+	char *symbol = malloc(sizeof(char));
 	int curr_row = 0;
 	int curr_col = 0;
-	u->num_of_x = 0;
-	u->num_of_o = 0;
 	
 	while(!feof(infile)){
 		if(curr_row == u->max_rows){
 			u->board_ptr = (char**)realloc(u->board_ptr, ((u->max_rows)+1)*sizeof(char*));
-			if(u->board_ptr == NULL) exit(1);
+			if(u->board_ptr == NULL){
+				fprintf(stderr, "Error: Unable to reallocate memory for board pointer\n");
+				cleanup_board(u);
+				free(symbol);
+				exit(1);
+			}
 			u->max_rows++;
 			(u->board_ptr)[curr_row] = (char*)malloc((u->max_cols)*sizeof(char)+1);
+			if(u->board_ptr[curr_row] == NULL){
+				fprintf(stderr, "Error: Unable to allocate memory for pointer to char\n");
+				cleanup_board(u);
+				free(symbol);
+				exit(1);
+			}
 		}
 		fscanf(infile, "%c", &symbol[0]);
 		if(symbol[0] == '.' || symbol[0] == 'x' || symbol[0] == 'o'){
 			if(curr_col == u->max_cols){
 				(u->board_ptr)[curr_row] = (char*)realloc((u->board_ptr)[curr_row], ((u->max_cols))*sizeof(char)+1);
-				if((u->board_ptr)[curr_row] == NULL) exit(1);
+				if((u->board_ptr)[curr_row] == NULL){
+					fprintf(stderr, "Error: Unable to reallocate memory for pointer to char\n");
+					cleanup_board(u);
+					free(symbol);
+					exit(1);
+				}
 				u->max_cols++;
 			}
 			(u->board_ptr)[curr_row][curr_col] = symbol[0];
@@ -77,10 +111,15 @@ void read_in_file(FILE *infile, board u){
 			curr_row++;
 			curr_col = 0;
 		}
-		count++;
 	}
 
 	(u->max_rows)--;
+	free(symbol);
+	// if((u->board_ptr)[0][0] != '.' && (u->board_ptr)[0][0] != 'x' && (u->board_ptr)[0][0] != 'o'){
+	// 	cleanup_board(u);
+	// 	fprintf(stderr, "Error: File empty or does not contain appropriate board format\n");
+	// 	exit(1);
+	// }
 }
 
 void write_out_file(FILE *outfile, board u){
@@ -189,22 +228,20 @@ char next_player(board u){
 }
 
 char current_winner(board u){
-	int x_is_winner = 0;
-	int o_is_winner = 0;
-	int empty_space = 0;
 	int pos_x, pos_y, count;
 	if(u->num_of_x == 0 && u->num_of_o == 0) return '.';
 	for(int x = u->max_rows - 1; x >= 0; x--){
-		if(x_is_winner && o_is_winner) break;
+		if(u->win_info[2] != -1 && u->win_info[5] != -1) break;
 		for(int y = 0; y < u->max_cols; y++){
 			if((u->board_ptr)[x][y] != '.'){
 
-				if((x_is_winner && (u->board_ptr)[x][y] == 'x') || (o_is_winner && (u->board_ptr)[x][y] == 'o')) continue;
+				if(((u->win_info)[2] != -1 && (u->board_ptr)[x][y] == 'x') || ((u->win_info)[5] != -1 && (u->board_ptr)[x][y] == 'o')) continue;
 
 				// down
 				pos_x = x;
 				count = 0;
 				for(int i = 0; i < 4; i++){
+					if(u->max_rows < 4) break;
 					if(pos_x >= u->max_rows) break;
 					if((u->board_ptr)[pos_x][y] == (u->board_ptr)[x][y]){
 						count++;
@@ -217,13 +254,11 @@ char current_winner(board u){
 					(u->win_info)[0] = x;
 					(u->win_info)[1] = y;
 					(u->win_info)[2] = 1;
-					x_is_winner = 1;
 					continue;
 				}else if(((u->board_ptr)[x][y] == 'o' || (u->board_ptr)[x][y] == 'O') && count ==4){
 					(u->win_info)[3] = x;
 					(u->win_info)[4] = y;
 					(u->win_info)[5] = 1;
-					o_is_winner = 1;
 					continue;
 				}
 
@@ -243,13 +278,11 @@ char current_winner(board u){
 					(u->win_info)[0] = x;
 					(u->win_info)[1] = y;
 					(u->win_info)[2] = 2;
-					x_is_winner = 1;
 					continue;
 				}else if(((u->board_ptr)[x][y] == 'o' || (u->board_ptr)[x][y] == 'O') && count ==4){
 					(u->win_info)[3] = x;
 					(u->win_info)[4] = y;
 					(u->win_info)[5] = 2;
-					o_is_winner = 1;
 					continue;
 				}
 			
@@ -258,6 +291,7 @@ char current_winner(board u){
 				pos_y = y;
 				count = 0;
 				for(int i = 0; i < 4; i++){
+					if(u->max_rows < 4) break;
 					if(pos_y >= u->max_cols) pos_y = 0;
 					if(pos_x >= u->max_rows) break;
 					if((u->board_ptr)[pos_x][pos_y] == (u->board_ptr)[x][y]){
@@ -272,13 +306,11 @@ char current_winner(board u){
 					(u->win_info)[0] = x;
 					(u->win_info)[1] = y;
 					(u->win_info)[2] = 4;
-					x_is_winner = 1;
 					continue;
 				}else if(((u->board_ptr)[x][y] == 'o' || (u->board_ptr)[x][y] == 'O') && count ==4){
 					(u->win_info)[3] = x;
 					(u->win_info)[4] = y;
 					(u->win_info)[5] = 4;
-					o_is_winner = 1;
 					continue;
 				}
 
@@ -287,8 +319,9 @@ char current_winner(board u){
 				pos_y = y;
 				count = 0;
 				for(int i = 0; i < 4; i++){
+					if(u->max_rows < 4) break;
 					if(pos_y >= u->max_cols) pos_y = 0;
-					if(pos_x >= u->max_rows) break;
+					if(pos_x >= u->max_rows || pos_x < 0) break;
 					if((u->board_ptr)[pos_x][pos_y] == (u->board_ptr)[x][y]){
 						count++;
 						pos_x--;
@@ -301,26 +334,23 @@ char current_winner(board u){
 					(u->win_info)[0] = x;
 					(u->win_info)[1] = y;
 					(u->win_info)[2] = 3;
-					x_is_winner = 1;
 					continue;
 				}else if(((u->board_ptr)[x][y] == 'o' || (u->board_ptr)[x][y] == 'O') && count ==4){
 					(u->win_info)[3] = x;
 					(u->win_info)[4] = y;
 					(u->win_info)[5] = 3;
-					o_is_winner = 1;
 					continue;
 				}
 
 			}
-			empty_space++;
 		}
 	}
 
-	if((x_is_winner && o_is_winner) || (!x_is_winner && !o_is_winner && empty_space == 0)){
+	if((u->win_info[2] != -1 && u->win_info[5] != -1) || (u->win_info[2] == -1 && u->win_info[5] == -1 && ((u->num_of_x + u->num_of_o) == (u->max_rows * u->max_cols)))){
 		return 'd';
-	}else if(x_is_winner){
+	}else if(u->win_info[2] != -1){
 		return 'x';
-	}else if(o_is_winner){
+	}else if(u->win_info[5] != -1){
 		return 'o';
 	}
 	return '.';
@@ -330,27 +360,44 @@ struct move read_in_move(board u){
 	struct move player_move;
 
 	printf("Player %c enter column to place your token: ",next_player(u)); //Do not edit this line
-	if(scanf("%d", &(player_move.column)) != 1) exit(1);
+	if(scanf("%d", &(player_move.column)) != 1){
+		cleanup_board(u);
+		fprintf(stderr, "Error: Invalid user input type - needs to be of type Int\n");
+		exit(1);
+	}
 	printf("Player %c enter row to rotate: ",next_player(u)); // Do not edit this line
-	if(scanf("%d", &(player_move.row)) != 1) exit(1);
+	if(scanf("%d", &(player_move.row)) != 1){
+		cleanup_board(u);
+		fprintf(stderr, "Error: Invalid user input type - needs to be of type Int\n");
+		exit(1);
+	}
 
 	return player_move;
 }
 
 int is_valid_move(struct move m, board u){
-	if (m.column < 1 || m.column > u->max_cols || m.row < -(u->max_rows) || m.row > u->max_rows) exit(1);
+	if (m.column < 1 || m.column > u->max_cols || m.row < -(u->max_rows) || m.row > u->max_rows) return 0;
 	if ((u->board_ptr)[0][(m.column)-1] == '.') return 1;
-	exit(1);
+	return 0;
 }
 
 char is_winning_move(struct move m, board u){
 
 	char **board_copy = (char**)malloc((u->max_rows) * sizeof(char*));
-	if(board_copy == NULL) exit(1);
+	if(board_copy == NULL){
+		fprintf(stderr, "Error: Unable to allocate memory for board copy pointer\n");
+		cleanup_board(u);
+		exit(1);
+	}
 
 	for(int i = 0; i < u->max_rows; i++){
 		board_copy[i] = (char*)malloc((u->max_cols) * sizeof(char)+1);
-		if(board_copy[i] == NULL) exit(1);
+		if(board_copy[i] == NULL){
+			cleanup_board(u);
+			free(board_copy);
+			fprintf(stderr, "Error: Unable to allocate memory for pointer to char\n");
+			exit(1);
+		}
 	}
 
 	for(int x = 0; x < u->max_rows; x++){
@@ -391,7 +438,6 @@ char is_winning_move(struct move m, board u){
 void play_move(struct move m, board u){
 	// rows 1,2 ... bottom to top
 	// column 1,2 ... left to right
-	int row_selected = u->max_rows - abs(m.row);
 	int possible_row;
 	int count = 1;
 	char current_token;
@@ -412,23 +458,23 @@ void play_move(struct move m, board u){
 
 	if(m.row != 0){
 		if(m.row > 0){ // right rotation
-			prev_token = (u->board_ptr)[row_selected][0];
+			prev_token = (u->board_ptr)[u->max_rows - abs(m.row)][0];
 			for(int y = 1; y < u->max_cols; y++){
-				current_token = (u->board_ptr)[row_selected][y];
-				(u->board_ptr)[row_selected][y] = prev_token;
+				current_token = (u->board_ptr)[u->max_rows - abs(m.row)][y];
+				(u->board_ptr)[u->max_rows - abs(m.row)][y] = prev_token;
 				prev_token = current_token;
 				if(y == (u->max_cols) - 1){
-					(u->board_ptr)[row_selected][0] = prev_token;
+					(u->board_ptr)[u->max_rows - abs(m.row)][0] = prev_token;
 				}
 			}
 		}else if(m.row < 0){ // left rotation
-			prev_token = (u->board_ptr)[row_selected][(u->max_cols) - 1];
+			prev_token = (u->board_ptr)[u->max_rows - abs(m.row)][(u->max_cols) - 1];
 			for(int y = (u->max_cols) - 2; y >= 0; y--){
-				current_token = (u->board_ptr)[row_selected][y];
-				(u->board_ptr)[row_selected][y] = prev_token;
+				current_token = (u->board_ptr)[u->max_rows - abs(m.row)][y];
+				(u->board_ptr)[u->max_rows - abs(m.row)][y] = prev_token;
 				prev_token = current_token;
 				if(y == 0){
-					(u->board_ptr)[row_selected][(u->max_cols) - 1] = prev_token;
+					(u->board_ptr)[u->max_rows - abs(m.row)][(u->max_cols) - 1] = prev_token;
 				}
 			}
 		}
